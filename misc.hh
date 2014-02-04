35ef7997bb99fca359e7cc06a56087110cb4aca2
bert hubert <bert.hubert@netherlabs.nl>
2014-02-04 11:19:33 +0100

    misc.hh wasn't in git?!

---
+++ misc.hh
@@ -0,0 +1,38 @@
+#pragma once
+#include <stdio.h>
+#include <string>
+#include <stdint.h>
+
+/** Rapid estimator of variance and mean of a series of doubles. 
+    API compatible with a, sadly, far slower boost::accumulator_set
+    doing the same thing*/
+class VarMeanEstimator
+{
+public:
+  VarMeanEstimator() : N(0), xTot(0), x2Tot(0) {}
+  void operator()(double val) 
+  {
+    ++N;
+    xTot += val;
+    x2Tot += val*val;
+  }
+  friend double mean(const VarMeanEstimator& vme);
+  friend double variance(const VarMeanEstimator& vme);
+private:
+  uint64_t N;
+  double xTot;
+  double x2Tot;
+};
+
+//! extract 'mean' from a VarMeanEstimator
+inline double mean(const VarMeanEstimator& vme)
+{
+  return vme.xTot/vme.N;
+}
+
+//! extract 'variance' from a VarMeanEstimator
+inline double variance(const VarMeanEstimator& vme) 
+{
+  return (vme.x2Tot - vme.xTot*vme.xTot/vme.N)/vme.N;
+}
+bool stringfgets(FILE* fp, std::string* line);
